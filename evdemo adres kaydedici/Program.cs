c75c330e36ab23fce0ad73b8ccadcf63a97cae4b
adem öcel <ademocel83@gmail.com>
2024-03-02 01:30:52 +0300

    create project

---
+++ evdemo adres kaydedici/Program.cs
@@ -0,0 +1,55 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace evdemo_adres_kaydedici;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filepatch = "/home/adem/İndirilenler/Zimmetler.pdf";
+        
+        string pdftext = read_pdf_full(filepatch);
+        string[] adresslist = list_customer_adress(pdftext).ToArray();
+
+        foreach (var item in adresslist)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+
+    static string read_pdf_full(string filepatch)
+    {
+        PdfReader reader = new PdfReader(filepatch);
+        string text = string.Empty;
+        for (int page = 1; page <= reader.NumberOfPages; page++)
+        {
+            text += PdfTextExtractor.GetTextFromPage(reader, page);
+        }
+        text = text.Replace('�', 'i');
+        reader.Close();
+        return text;
+    }
+
+
+    static List<string> list_customer_adress(string text)
+    {
+        string[] splited_text = text.Split("\n");
+        List<string> adresslist = new();
+
+        for (int i = 0; i < splited_text.Length; i++)
+        {
+            string line = splited_text[i];
+            //bir sonraki satırı alır
+            if (line.StartsWith("Alıcı Adı :"))
+            {
+                adresslist.Add(splited_text[i + 1]);
+            }
+        }
+        return adresslist;
+    }
+
+
+
+
+}
